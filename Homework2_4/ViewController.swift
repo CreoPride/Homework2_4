fbeb8128f80b7d82f244ec5449c052754679569d
Konstantin Fomenkov <kolij2005@gmail.com>
2023-04-02 17:40:39 +0300

    Initial Commit

---
+++ Homework2_4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework2_4
+//
+//  Created by Konstantin Fomenkov on 02.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
